10acd6f8eed95481572d86e20072c6de9c6c3e90
Andrey Sloboda <sloboda.am@gmail.com>
2020-07-07 13:01:41 +0300

    Complete transaction view

--- app/containers/TransactionConainer.js
+++ app/containers/TransactionConainer.js
@@ -1,8 +1,16 @@
 import React, { useState } from 'react';
-import { Image, View, StyleSheet } from 'react-native';
+import {
+  Image,
+  Text,
+  View,
+  StyleSheet
+} from 'react-native';
 
+import { palette } from 'helpers';
 import Switcher from 'components/Switcher';
 import TransactionCell from 'components/Cell/TransactionCell';
+
+const DateHeading = ({ text }) => <Text style={styles.date}>{text}</Text>;
 
 const TransactionContainer = () => {
   const [view, setView] = useState('Transactions');
@@ -16,9 +24,16 @@ const TransactionContainer = () => {
   return (
     <View style={styles.container}>
       <Switcher items={['Transactions', 'Settings']} active={view} onPress={handleCurrencyChange} />
-      <View style={styles.transactionList}>
+      <View>
+        <DateHeading text="Yesterday" />
         <TransactionCell name="...xJw82" value="+ 0.0000012 BTC" sub="9:36 am" note="~12.01 USD" image={image} status="done" />
         <TransactionCell name="Super Store" value="- 0.0000137 BTC" sub="12:42 pm" note="~ 126.45 USD" image={image} status="pending" />
+
+        <DateHeading text="Sunday, 20 Feb" />
+        <TransactionCell name="...xJw23k" value="- 0.000212 BTC" sub="9:36 am" note="~126.45 USD" image={image} status="error" />
+        
+        <DateHeading text="Saturday, 19 Feb" />
+        <TransactionCell name="Pizza Hut" value="- 0.0000137 BTC" sub="4:11 am" note="~ 126.45 USD" image={image} status="done" />
       </View>
     </View>
   )
@@ -26,12 +41,18 @@ const TransactionContainer = () => {
 
 const styles = StyleSheet.create({
   container: { marginTop: 24 },
-  transactionList: { marginTop: 16 },
   image: {
     width: 32,
     height: 32,
     borderRadius: 16,
   },
+  date: {
+    marginTop: 16,
+    fontSize: 13,
+    lineHeight: 20,
+    fontWeight: '600',
+    color: palette.twilight,
+  }
 })
 
 
